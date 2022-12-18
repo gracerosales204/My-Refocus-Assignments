65f425b8afdf304175e14a33dd3c7731d30d51e6
unknown <gracerosales204@gmail.com>
2022-12-18 15:11:32 +0800

    First New Assignment: BMI Calculator

---
+++ bmi_calculator.js
@@ -0,0 +1,25 @@
+
+// how to calculate BMI
+    function BMI(height, weight){
+    let bmi = weight/Math.pow(height, 2);
+    return bmi;
+    }
+    let height = 1.79832;
+        let weight = 70;
+         
+        // Function call
+        let bmi = BMI(height, weight);
+        process.stdout.write("Your BMI is " + bmi + ". It falls within ");
+         
+        // Conditions to find out BMI category
+        if (bmi < 18.5)
+            console.log("underweight range");
+ 
+        else if (bmi >= 18.5 && bmi < 24.9)
+            console.log("normal and healthy weight range");
+ 
+        else if (bmi >= 24.9 && bmi < 30)
+           console.log("overweight range");
+ 
+        else if (bmi >= 30)
+            console.log("obese range");
